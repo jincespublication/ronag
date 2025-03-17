fac85c63f651d1aa24ec9ac00503dcde650837f7
Hubert <ansahhubert8588@gmail.com>
2025-03-17 15:59:57 +0000

    Deploying to Render

--- ronagapp/urls.py
+++ ronagapp/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path("index.html", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("", views.index, name="index"),
-    path("about.html", views.about, name="about"),
-    path("contact.html", views.contact, name="contact"),
-    path("article.html", views.article, name="article"),
-    path("news.html", views.news, name="news"),
-    path("singleNews.html/<int:post_id>", views.singleNews, name="singleNews")
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("article", views.article, name="article"),
+    path("news", views.news, name="news"),
+    path("singleNews/<int:post_id>", views.singleNews, name="singleNews")
 ]
